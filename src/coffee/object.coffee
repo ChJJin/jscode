789866420485d05908e7ab5345d4636d2e1d9159
ChJJin <308366504@qq.com>
2014-03-31 22:25:06 +0800

    object

---
+++ src/coffee/object.coffee
@@ -0,0 +1,171 @@
+J = @J ?= {}
+
+argsClass   = '[object Arguments]'
+arrayClass  = '[object Array]'
+boolClass   = '[object Boolean]'
+dateClass   = '[object Date]'
+numberClass = '[object Number]'
+objectClass = '[object Object]'
+regexpClass = '[object RegExp]'
+stringClass = '[object String]'
+
+toString = Object.prototype.toString
+hasOwnProperty = Object.prototype.hasOwnProperty
+
+# /*
+#  * Checks if the specified `key` exists as a
+#  * direct property of `obj`, instead of an
+#  * inherited property.
+#  */
+J.has = (obj, key)->
+  has = hasOwnProperty.call(obj, key)
+
+# /*
+#  * check if `value` is a number
+#  * note: 'NaN' is considered a number
+#  */
+J.isNumber = (value)->
+  isNumber = (typeof value is 'number') or (toString.call(value) is numberClass)
+
+# /*
+#  * check if `value` is a string
+#  */
+J.isString = (value)->
+  isString = (typeof value is 'string') or (toString.call(value) is stringClass)
+
+# /*
+#  * check if `value` is a boolean value
+#  */
+J.isBoolean = (value)->
+  isBoolean = (value is false) or
+      (value is true) or
+      (toString.call(value) is boolClass)
+
+# /*
+#  * check if `value` is a date
+#  * note: typeof date is a object
+#  */
+J.isDate = (obj)->
+  isDate = toString.call(obj) is dateClass
+
+# /*
+#  * check if `value` is a regular expression
+#  * note: typeof regexp is a object
+#  */
+J.isRegExp = (obj)->
+  isRegExp = toString.call(obj) is regexpClass
+
+# /*
+#  * check if `value` is a `arguments` object
+#  */
+J.isArguments = (obj)->
+  isArguments = (typeof obj is 'object') and (toString.call(obj) is argsClass)
+
+# /*
+#  * check if `value` is a `function` object
+#  */
+J.isFunction = (obj)->
+  isFunction = typeof obj is 'function'
+
+# /*
+#  * check if `value` is a `array` object
+#  */
+J.isArray = Array.isArray || (obj)->
+  isArray = obj and
+      (typeof obj is 'object') and
+      (typeof obj.length is 'number') and
+      (toString.call(obj) is arrayClass)
+
+# /*
+#  * check if `value` is a object
+#  * (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
+#  */
+J.isObject = (obj)->
+  isObject = obj and (typeof obj is 'object') or (typeof obj is 'function')
+
+# /*
+#  * check if `value` is a plain object
+#  * note: return true only if `value` is object like `{}`
+#  */
+J.isPlainObject = (obj)->
+  isPlainObject = toString.call(obj) is objectClass
+
+# /*
+#  * check if `value` is empty
+#  * note: if arrays, strings, arguments with length of `0`,
+#  *       or objects without own keys, are considered `empty`
+#  */
+J.isEmpty = (value)->
+  if not value then return true
+  if J.isArray(value) or J.isString(value) or J.isArguments(value) then return value.length is 0
+  for key of value when J.has(value, key) then return false
+  return true
+
+# /*
+#  * get the specified `key` of `data`
+#  * note: `key` can be specified like `a.b[0].c`
+#  *       then return the value of data.a.b[0].c
+#  */
+J.pick = (data, key = '', defaultValue)->
+  if not J.isObject(data) then return defaultValue
+
+  parts = key.split /(?:\[(\d+)\]\.?)|\./g
+  for k in parts when k? and (k isnt "")
+    data = data[k]
+    if not data?
+      return defaultValue
+
+  return data
+
+# /*
+#  * merge source data to destination by turns.
+#  * if `deep` is false (default `true`), then the
+#  * value on the first layer from the source data
+#  * will directly overwrite the destination data.
+#  *
+#  * @example
+#  *
+#  * J.extend({a: {b: 1, c: 2}}, {a: {b: 3}}, false);
+#  * // => {a: {b: 3}}
+#  *
+#  * J.extend({a: {b: 1, c: 2}}, {a: {b: 3}}, true);
+#  * // => {a: {b: 3, c: 2}}
+#  */
+J.extend = (dest, src..., deep)->
+  if not J.isBoolean(deep)
+    [dest, src...] = arguments
+    deep = true
+  for source in src
+    for key, value of source when J.has source, key
+      if deep
+        if not dest[key]? then dest[key] = {}
+        if J.isArray(value) and J.isEmpty(dest[key]) then dest[key] = []
+
+        if J.isObject value
+          J.extend dest[key], value, deep
+        else
+          dest[key] = value
+      else
+        dest[key] = value
+  return dest
+
+# /*
+#  * join key with divider of obj.
+#  *
+#  * @example
+#  *
+#  * J.join({a: {b: 1, c: 2}});
+#  * // => {'a.b': 1, 'a.c': 2}
+#  */
+J.join = (obj, divider = '.', prefix = '')->
+  ret = {}
+  if J.isArray(obj) then ret = []
+  for key, value of obj when J.has obj, key
+    if J.isObject(value)
+      deepRet = J.join value, divider, "#{prefix}#{key}#{divider}"
+      ret = J.extend ret, deepRet, true
+    else
+      ret["#{prefix}#{key}"] = value
+  return ret
+
+module?.exports = @J
---
+++ src/js/object.js
@@ -0,0 +1,183 @@
+(function() {
+  var J, argsClass, arrayClass, boolClass, dateClass, hasOwnProperty, numberClass, objectClass, regexpClass, stringClass, toString,
+    __slice = [].slice;
+
+  J = this.J != null ? this.J : this.J = {};
+
+  argsClass = '[object Arguments]';
+
+  arrayClass = '[object Array]';
+
+  boolClass = '[object Boolean]';
+
+  dateClass = '[object Date]';
+
+  numberClass = '[object Number]';
+
+  objectClass = '[object Object]';
+
+  regexpClass = '[object RegExp]';
+
+  stringClass = '[object String]';
+
+  toString = Object.prototype.toString;
+
+  hasOwnProperty = Object.prototype.hasOwnProperty;
+
+  J.has = function(obj, key) {
+    var has;
+    return has = hasOwnProperty.call(obj, key);
+  };
+
+  J.isNumber = function(value) {
+    var isNumber;
+    return isNumber = (typeof value === 'number') || (toString.call(value) === numberClass);
+  };
+
+  J.isString = function(value) {
+    var isString;
+    return isString = (typeof value === 'string') || (toString.call(value) === stringClass);
+  };
+
+  J.isBoolean = function(value) {
+    var isBoolean;
+    return isBoolean = (value === false) || (value === true) || (toString.call(value) === boolClass);
+  };
+
+  J.isDate = function(obj) {
+    var isDate;
+    return isDate = toString.call(obj) === dateClass;
+  };
+
+  J.isRegExp = function(obj) {
+    var isRegExp;
+    return isRegExp = toString.call(obj) === regexpClass;
+  };
+
+  J.isArguments = function(obj) {
+    var isArguments;
+    return isArguments = (typeof obj === 'object') && (toString.call(obj) === argsClass);
+  };
+
+  J.isFunction = function(obj) {
+    var isFunction;
+    return isFunction = typeof obj === 'function';
+  };
+
+  J.isArray = Array.isArray || function(obj) {
+    var isArray;
+    return isArray = obj && (typeof obj === 'object') && (typeof obj.length === 'number') && (toString.call(obj) === arrayClass);
+  };
+
+  J.isObject = function(obj) {
+    var isObject;
+    return isObject = obj && (typeof obj === 'object') || (typeof obj === 'function');
+  };
+
+  J.isPlainObject = function(obj) {
+    var isPlainObject;
+    return isPlainObject = toString.call(obj) === objectClass;
+  };
+
+  J.isEmpty = function(value) {
+    var key;
+    if (!value) {
+      return true;
+    }
+    if (J.isArray(value) || J.isString(value) || J.isArguments(value)) {
+      return value.length === 0;
+    }
+    for (key in value) {
+      if (J.has(value, key)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  J.pick = function(data, key, defaultValue) {
+    var k, parts, _i, _len;
+    if (key == null) {
+      key = '';
+    }
+    if (!J.isObject(data)) {
+      return defaultValue;
+    }
+    parts = key.split(/(?:\[(\d+)\]\.?)|\./g);
+    for (_i = 0, _len = parts.length; _i < _len; _i++) {
+      k = parts[_i];
+      if (!((k != null) && (k !== ""))) {
+        continue;
+      }
+      data = data[k];
+      if (data == null) {
+        return defaultValue;
+      }
+    }
+    return data;
+  };
+
+  J.extend = function() {
+    var deep, dest, key, source, src, value, _i, _j, _len;
+    dest = arguments[0], src = 3 <= arguments.length ? __slice.call(arguments, 1, _i = arguments.length - 1) : (_i = 1, []), deep = arguments[_i++];
+    if (!J.isBoolean(deep)) {
+      dest = arguments[0], src = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+      deep = true;
+    }
+    for (_j = 0, _len = src.length; _j < _len; _j++) {
+      source = src[_j];
+      for (key in source) {
+        value = source[key];
+        if (J.has(source, key)) {
+          if (deep) {
+            if (dest[key] == null) {
+              dest[key] = {};
+            }
+            if (J.isArray(value) && J.isEmpty(dest[key])) {
+              dest[key] = [];
+            }
+            if (J.isObject(value)) {
+              J.extend(dest[key], value, deep);
+            } else {
+              dest[key] = value;
+            }
+          } else {
+            dest[key] = value;
+          }
+        }
+      }
+    }
+    return dest;
+  };
+
+  J.join = function(obj, divider, prefix) {
+    var deepRet, key, ret, value;
+    if (divider == null) {
+      divider = '.';
+    }
+    if (prefix == null) {
+      prefix = '';
+    }
+    ret = {};
+    if (J.isArray(obj)) {
+      ret = [];
+    }
+    for (key in obj) {
+      value = obj[key];
+      if (J.has(obj, key)) {
+        if (J.isObject(value)) {
+          deepRet = J.join(value, divider, "" + prefix + key + divider);
+          ret = J.extend(ret, deepRet, true);
+        } else {
+          ret["" + prefix + key] = value;
+        }
+      }
+    }
+    return ret;
+  };
+
+  if (typeof module !== "undefined" && module !== null) {
+    module.exports = this.J;
+  }
+
+}).call(this);
